3ca704959187014b37adee4d62dd6a85169391f2
Ant0yne <antoine.soliv@gmail.com>
2024-03-08 18:18:46 +0100

    add-delete-offer-route_add-token-check-put-offer-id

--- routes/offer.js
+++ routes/offer.js
@@ -55,75 +55,6 @@ router.post(
 );
 
 /**
- * Modify an existing offer
- */
-router.put("/offer/:id", isAuthenticated, fileUpload(), async (req, res) => {
-	try {
-		const isBodyPopulate = (obj) => {
-			for (const prop in obj) {
-				if (Object.hasOwn(obj, prop)) {
-					return true;
-				}
-			}
-			return false;
-		};
-
-		if (isBodyPopulate(req.body) === false) {
-			return res.status(400).json({
-				message:
-					"Please change at least one information from your offer before validate.",
-			});
-		}
-
-		const offerID = req.params.id;
-		const offerToModify = await Offer.findOne({ _id: offerID });
-
-		if (offerToModify) {
-			if (req.body.title) {
-				offerToModify.product_name = req.body.title;
-			}
-			if (req.body.description) {
-				offerToModify.product_description = req.body.description;
-			}
-			if (Number(req.body.price)) {
-				offerToModify.product_price = req.body.price;
-			}
-			if (req.body.brand) {
-				offerToModify.product_details[0] = { MARQUE: req.body.brand };
-			}
-			if (req.body.size) {
-				offerToModify.product_details[1] = { TAILLE: req.body.size };
-			}
-			if (req.body.condition) {
-				offerToModify.product_details[2] = { ÉTAT: req.body.condition };
-			}
-			if (req.body.color) {
-				offerToModify.product_details[3] = { COULEUR: req.body.color };
-			}
-			if (req.body.city) {
-				offerToModify.product_details[4] = { EMPLACEMENT: req.body.city };
-			}
-			if (req.files) {
-				const newFile = await cloudinaryFunc.deleteCreate(
-					req.files.picture,
-					offerToModify.product_image.public_id
-				);
-				// console.log("Etape 4 : ", newFile);
-				offerToModify.product_image = newFile;
-			}
-
-			await offerToModify.save();
-
-			return res.status(200).json(offerToModify);
-		} else {
-			return res.status(404).json({ message: "This offer doesn't exist." });
-		}
-	} catch (error) {
-		return res.status(500).json({ message: error.message });
-	}
-});
-
-/**
  *  Return an array with the offers (according to the query) from the DB
  */
 router.get("/offers", async (req, res) => {
@@ -186,4 +117,99 @@ router.get("/offers/:id", async (req, res) => {
 	}
 });
 
+/**
+ * Modify an existing offer
+ */
+router.put("/offer/:id", isAuthenticated, fileUpload(), async (req, res) => {
+	try {
+		const isBodyPopulate = (obj) => {
+			for (const prop in obj) {
+				if (Object.hasOwn(obj, prop)) {
+					return true;
+				}
+			}
+			return false;
+		};
+
+		if (isBodyPopulate(req.body) === false) {
+			return res.status(400).json({
+				message:
+					"Please change at least one information from your offer before validate.",
+			});
+		}
+
+		const offerID = req.params.id;
+		const offerToModify = await Offer.findOne({ _id: offerID }).populate(
+			"owner"
+		);
+
+		if (!offerToModify) {
+			return res.status(404).json({ message: "This offer doesn't exist." });
+		} else if (offerToModify.owner.token !== req.user.token) {
+			return res.status(401).json({ error: "Unauthorized to do this action." });
+		} else {
+			if (req.body.title) {
+				offerToModify.product_name = req.body.title;
+			}
+			if (req.body.description) {
+				offerToModify.product_description = req.body.description;
+			}
+			if (Number(req.body.price)) {
+				offerToModify.product_price = req.body.price;
+			}
+			if (req.body.brand) {
+				offerToModify.product_details[0] = { MARQUE: req.body.brand };
+			}
+			if (req.body.size) {
+				offerToModify.product_details[1] = { TAILLE: req.body.size };
+			}
+			if (req.body.condition) {
+				offerToModify.product_details[2] = { ÉTAT: req.body.condition };
+			}
+			if (req.body.color) {
+				offerToModify.product_details[3] = { COULEUR: req.body.color };
+			}
+			if (req.body.city) {
+				offerToModify.product_details[4] = { EMPLACEMENT: req.body.city };
+			}
+			if (req.files) {
+				const newFile = await cloudinaryFunc.deleteCreate(
+					req.files.picture,
+					offerToModify.product_image.public_id
+				);
+				// console.log("Etape 4 : ", newFile);
+				offerToModify.product_image = newFile;
+			}
+
+			await offerToModify.save();
+
+			return res.status(200).json(offerToModify);
+		}
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
+});
+
+router.delete("/offer/:id", isAuthenticated, async (req, res) => {
+	try {
+		const offerID = req.params.id;
+		const offerToDelete = await Offer.findOne({ _id: offerID }).populate(
+			"owner"
+		);
+
+		if (!offerToDelete) {
+			return res.status(404).json({ message: "This offer doesn't exist." });
+		} else if (offerToDelete.owner.token !== req.user.token) {
+			return res.status(401).json({ error: "Unauthorized to do this action." });
+		} else {
+			await offerToDelete.deleteOne();
+
+			return res.status(200).json({
+				message: `Your offer ${offerToDelete.product_name} was successfully deleted.`,
+			});
+		}
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
+});
 module.exports = router;
